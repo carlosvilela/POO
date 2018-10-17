88ea858c12972c3c762bf2d4fa16ced8e5e75805
Carlos Vilela <carlosvilela@users.noreply.github.com>
2018-10-17 14:40:11 -0300

    Main

--- APS/Main.java
+++ APS/Main.java
@@ -11,6 +11,18 @@ public class Main {
 		telaInicial.exibirMenu(enumEstilo.MENU.ordinal(), null);
 		telaInicial.exibirMenu(enumEstilo.TITULO.ordinal(), enumOpcao.CADASTRAR_PF.ordinal());
 		telaInicial.escolherOpcao();
+		
+		PessoaFisica pFisica = new PessoaFisica();
+		pFisica.setNome("Carlos");
+		pFisica.setCpf("12345678909");
+		pFisica.setEndereco("Av. sem nome, 0009");
+		System.out.println(pFisica.imprimir());
+
+		PessoaJuridica pJuridica = new PessoaJuridica();
+		pJuridica.setNome("Carlos");
+		pJuridica.setCnpj("12345678909000001");
+		pJuridica.setEndereco("Av. sem nome, 0009");
+		System.out.println(pJuridica.imprimir());
 
 	}
 
